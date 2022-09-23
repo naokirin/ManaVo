1750b05bc5d659b1fbf07aa705c4ff3bf90f3a2c
naokirin <naoki.rin186@gmail.com>
2022-09-23 21:38:06 +0900

    Redesign sound item

--- lib/pages/course/sound_item.dart
+++ lib/pages/course/sound_item.dart
@@ -13,13 +13,35 @@ class SoundItem extends StatelessWidget {
     return Card(
         elevation: 10,
         child: ListTile(
-            leading: const Icon(Icons.check_circle,
-                color: Colors.green, size: 24.0), // TODO: Set if done
-            title: Text(sound.name),
-            subtitle: Text(sound.length),
-            onTap: () {
-              GoRouter.of(context)
-                  .push("/course/$courseId/player/${sound.id}");
-            }));
+            tileColor: Colors.blueGrey.shade50,
+            trailing: SizedBox(
+                width: 45.0,
+                height: 80.0,
+                child: Column(children: [
+                  const Align(
+                      alignment: Alignment.centerRight,
+                      child: Icon(Icons.check_circle,
+                          color: Colors.green,
+                          size: 24.0)), // TODO: Set if done
+                  Align(
+                      alignment: Alignment.bottomLeft,
+                      child: Padding(
+                          padding: const EdgeInsets.only(top: 10.0),
+                          child: Text(sound.length,
+                              style: const TextStyle(fontSize: 15, color: Colors.black54))))
+                ])),
+            title: SizedBox(
+              height: 80.0,
+              child: Padding(
+                  padding: const EdgeInsets.only(top: 12.0),
+                  child: Text(sound.name)),
+            ),
+            onTap: () => goPlayerPage(context)));
   }
+
+  void goPlayerPage(BuildContext context) {
+    GoRouter.of(context).push(playerPath);
+  }
+
+  String get playerPath => "/course/$courseId/player/${sound.id}";
 }
