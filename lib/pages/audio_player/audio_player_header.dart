78e479b80b1c0493171040badc4c6b47b07735e7
naokirin <naoki.rin186@gmail.com>
2022-10-04 19:06:59 +0900

    Show listened count

--- lib/pages/audio_player/audio_player_header.dart
+++ lib/pages/audio_player/audio_player_header.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:manavo/models/course.dart';
 import 'package:manavo/models/lesson.dart';
+import 'package:manavo/providers/listened.dart';
 
-class AudioPlayerHeader extends StatelessWidget {
+class AudioPlayerHeader extends ConsumerWidget {
   final Course? course;
   final Lesson? lesson;
 
@@ -10,7 +12,8 @@ class AudioPlayerHeader extends StatelessWidget {
       {super.key, required this.course, required this.lesson});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final listened = ref.watch(listenedProvider.notifier);
     return Container(
         width: double.infinity,
         decoration: BoxDecoration(
@@ -44,7 +47,39 @@ class AudioPlayerHeader extends StatelessWidget {
                   Padding(
                       padding: const EdgeInsets.all(20.0),
                       child: Text(lesson?.description ?? '',
-                          style: const TextStyle(color: Colors.white)))
+                          style: const TextStyle(color: Colors.white))),
+                  FutureBuilder(
+                      future:
+                          listened.listenedCount(lessonId: lesson?.id ?? ''),
+                      builder: (context, snapshot) {
+                        if (snapshot.hasData) {
+                          return Column(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Container(height: 60),
+                                const Text('これまでの再生回数',
+                                    style: TextStyle(
+                                        fontSize: 14,
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.white)),
+                                Row(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.center,
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      Text(snapshot.data.toString(),
+                                          style: const TextStyle(
+                                              fontSize: 16,
+                                              color: Colors.white)),
+                                      Container(width: 5),
+                                      const Text('回',
+                                          style: TextStyle(color: Colors.white))
+                                    ])
+                              ]);
+                        }
+                        return Container();
+                      })
                 ]))));
   }
 
