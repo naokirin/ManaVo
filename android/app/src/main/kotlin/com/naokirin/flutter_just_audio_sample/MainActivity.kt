7ebe390755131e7ff6faaa01843f86c91a0a5a77
naokirin <naoki.rin186@gmail.com>
2022-10-23 01:11:41 +0900

    Add privacy policy page on root

--- android/app/src/main/kotlin/com/naokirin/flutter_just_audio_sample/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.naokirin.manavo
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/utils/global/snack_bar.dart
+++
@@ -1,5 +0,0 @@
-import 'package:manavo/utils/global/scaffold_key.dart';
-
-void removeCurrentSnackBar() {
-  scaffoldKey.currentState!.removeCurrentSnackBar();
-}
